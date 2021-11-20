ed68b0a88fd524fb034feb4961911014219fecce
curtis <hong.curtis@gmail.com>
2021-11-20 13:47:08 -0800

    near completion

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,53 +72,28 @@ function generatePasswordOptions() {
       potentialCharacters = potentialCharacters.concat(uppercase)
       confirmedCharacters.push(random(uppercase))
     }
+
+    for (let i = 0; i < passwordOptions.length; i++) {
+      var potentialCharacters = random(potentialCharacters)
+      endResult.push(potentialCharacters)
+    }
+    for (let i = 0; i<confirmedCharacters.length; i++) {
+      endResult[i] = confirmedCharacters[i]   
+    }
+    return endResult.join(" ")
   }
 
 
-  for (let i = 0; i < passwordOptions.length; i++) {
-    
-    }
-  
+
   
 
 
-//   var lowercase = window.confirm("Would you like to use lowercase letters?");
-//   if (lowercase) {
-//     passwordCharSet += key_strings.lowercase;
-//   };
-
-//   var uppercase = window.confirm("Would you like to use uppercase letters?");
-//   if (uppercase) {
-//     passwordCharSet += key_strings.uppercase;
-//   };
-
-//   var symbols = window.confirm("Would you like to use symbols?");
-//   if (symbols) {
-//     passwordCharSet += key_strings.symbol;
-//   };
-
-//   var numbers = window.confirm("Would you like to use numbers?");
-//   if (numbers) {
-//     passwordCharSet += key_strings.number;
-//   };
-//   var password = "";
-//   for (let i = 0; i < length; i++) {
-//     password += passwordCharSet[Math.floor(Math.random() * passwordChoices.length)]
-//   }
-  
-//   // Validate the input
-
-
-//   // Display Password
-//   return password;
-
-// }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePasswordOptions();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
