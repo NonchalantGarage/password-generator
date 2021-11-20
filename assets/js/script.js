e7090143b60f4d7b1e930e70e3510e7c94251515
curtis <hong.curtis@gmail.com>
2021-11-20 08:35:54 -0800

    added functions to generate random characters and placed them into an object, prompt for password lenght variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,52 @@
+
+// Create object for functions created below for random criteria
+
+var randomFunction ={
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSumbol
+}
+
+// Functions to generate random based on criteria
+
+function getRandomLower(){
+  return String.fromCharCode(Math.floor(Math.random() *26) + 97);
+}
+function getRandomUpper(){
+  return String.fromCharCode(Math.floor(Math.random() *26) + 65);
+}
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() *10) + 48);
+}
+function getRandomSumbol(){
+    const symbols = "!@#$%^&*(){}[]=<>,.";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
+
+var generatePassword = function (){
+  // Prompt welcome message
+  window.alert("Welcome to Password Generator. You're on your way to a new shiny password!");
+
+
+  // Pormpt to choose Length between 8<128
+  var passwordLength = window.prompt("Please choose a password length between 8 and 128 characters");
+// convert password lenght input to integer
+  passwordLength = parseInt(passwordLength);
+// validate if the password is between 8 * 128 characters
+  if (passwordLength < 8 || passwordLength > 128){
+    window.alert("You need to provide a valid answer! Please try again.");
+  }
+
+  console.log(passwordLength);
+
+
 // Prompt the user for password criteria
-// Length between 8<128
+
+
+}
 // Lowecase, uppercase, numbers, special characters
 // Validate the input
 // generate password based on criteria 
@@ -7,19 +54,7 @@
 
 // 4. Display password 
 
-var generatePassword = function (){
-  var chars = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passwordLength = 12;
-  var PW = "";
-  
-  for (var i = 0; i <= passwordLength; i++){
-    var randomNumber = Math.floor(Math.random() * chars.length);
-    return PW + gitchars.substring(randomNumber, randomNumber +1);
-  }
-}
 
-
-  
 
 
 // Get references to the #generate element
